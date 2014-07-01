8500c06c16fe44c9a75db8cbe4a5d5372e2bd584
Valerino Wijaya <valerino@gmail.com>
2014-07-01 11:55:48 +0700

    Initial Commit

---
+++ NSUserDefaults,Seques,Protocols Challenge/VWViewController.h
@@ -0,0 +1,13 @@
+//
+//  VWViewController.h
+//  NSUserDefaults,Seques,Protocols Challenge
+//
+//  Created by Valerino on 7/1/14.
+//  Copyright (c) 2014 VW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VWViewController : UIViewController
+
+@end
---
+++ NSUserDefaults,Seques,Protocols Challenge/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NSUserDefaults,Seques,Protocols Challenge
+//
+//  Created by Valerino on 7/1/14.
+//  Copyright (c) 2014 VW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VWAppDelegate class]));
+    }
+}
