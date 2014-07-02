3b31113750471851a7f89b1c14bbfef0e428c044
Valerino Wijaya <valerino@gmail.com>
2014-07-02 13:41:11 +0700

    Up to lecture 174. Finished the challenge.

--- NSUserDefaults,Seques,Protocols Challenge/VWViewController.m
+++ NSUserDefaults,Seques,Protocols Challenge/VWViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "VWViewController.h"
+#import "VWCreateAccountViewController.h"
 
 @interface VWViewController ()
 
@@ -18,6 +19,9 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.usernameLabel.text = [[NSUserDefaults standardUserDefaults] objectForKey:USER_NAME];
+    self.passwordLabel.text = [[NSUserDefaults standardUserDefaults] objectForKey:USER_PASSWORD];
 }
 
 - (void)didReceiveMemoryWarning
